09b06e3ce01d7fad9dc5fbcf3596d356345b3ee4
Wilson.L <klwilsonlau@gmail.com>
2019-06-10 18:42:14 +0800

    new ip

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@
 
 
 /usr/bin/tmux new-session -d -s dask_w
-/usr/bin/tmux send-keys -t dask_w "/home/ubuntu/anaconda/bin/dask-worker tcp://34.207.165.175:8786" C-m
+/usr/bin/tmux send-keys -t dask_w "/home/ubuntu/anaconda/bin/dask-worker tcp://3.87.226.135:8786" C-m
