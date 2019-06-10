20d77a6e7efdcefc120f99ceb926a382ea0da264
Wilson.L <klwilsonlau@gmail.com>
2019-06-10 18:17:50 +0800

    init

---
+++ awsUserData.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -x
+
+#check log using tail -n 100 /var/log/syslog
+#check full log using cat /var/log/syslog
+
+sleep 10
+
+cd /home/ubuntu
+git clone https://github.com/klwlau/
+
+cd 
+sudo -u ubuntu bash setup.sh
