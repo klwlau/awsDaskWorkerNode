75e7421f2ee119038bed76d836c1d2dc12267abb
Wilson.L <klwilsonlau@gmail.com>
2019-06-10 18:25:35 +0800

    add ip

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@
 
 
 /usr/bin/tmux new-session -d -s dask_s
-/usr/bin/tmux send-keys -t dask_s "/home/ubuntu/anaconda/bin/dask-wroker tcp://54.152.216.9:8786" C-m
+/usr/bin/tmux send-keys -t dask_s "/home/ubuntu/anaconda/bin/dask-wroker tcp://34.207.165.175:8786" C-m
