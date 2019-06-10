d07142c3439bcff9c713338f64d3867920272ec9
Wilson.L <klwilsonlau@gmail.com>
2019-06-10 18:30:15 +0800

    rename tmux sec

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -x
 
 
-/usr/bin/tmux new-session -d -s dask_s
-/usr/bin/tmux send-keys -t dask_s "/home/ubuntu/anaconda/bin/dask-wroker tcp://34.207.165.175:8786" C-m
+/usr/bin/tmux new-session -d -s dask_w
+/usr/bin/tmux send-keys -t dask_w "/home/ubuntu/anaconda/bin/dask-wroker tcp://34.207.165.175:8786" C-m
