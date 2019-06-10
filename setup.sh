8db3b76510eb1a0a7f1fcb4c075da48209d8389a
Wilson <klwilsonlau@gmail.com>
2019-06-10 22:34:33 +0800

    update

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@
 
 
 /usr/bin/tmux new-session -d -s dask_w
-/usr/bin/tmux send-keys -t dask_w "/home/ubuntu/anaconda/bin/dask-worker tcp://34.227.112.33:8786 --no-nanny" C-m
+/usr/bin/tmux send-keys -t dask_w "/home/ubuntu/anaconda/bin/dask-worker tcp://asdfargdask.ddns.net:8786 --no-nanny" C-m
